e49fa7ee44d5dfbc4e87a3f4cd279cb3e7833c16
ikunming <1797202841@qq.com>
2024-11-14 15:38:37 +0800

    Add files via upload

---
+++ partner-backend-master/yupao-backend-master/src/main/java/com/yupi/yupao/mapper/TeamMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yupao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.yupao.model.domain.Team;
+
+/**
+ * 队伍 Mapper
+ *
+ * 
+ * 
+ */
+public interface TeamMapper extends BaseMapper<Team> {
+
+}
+
+
+
+
---
+++ partner-backend-master/yupao-backend-master/src/main/java/com/yupi/yupao/mapper/UserTeamMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.yupao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.yupao.model.domain.UserTeam;
+
+/**
+ * 用户队伍 Mapper
+ *
+ * 
+ * 
+ */
+public interface UserTeamMapper extends BaseMapper<UserTeam> {
+
+}
+
+
+
+
